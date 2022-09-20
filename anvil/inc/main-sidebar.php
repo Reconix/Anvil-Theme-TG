2fa9cab1b498db23d001cd1491a482ce86bbc5a3
Reconix <Reconix@users.noreply.github.com>
2022-09-20 23:41:10 +0100

    and so it begins...
    Added the basic theme and correct paths, at the moment everything is spat out into the main content area. The other versions of Anvil don't do this.

---
+++ anvil/inc/main-sidebar.php
@@ -0,0 +1,94 @@
+<!-- Left side column. contains the logo and sidebar -->
+<aside class="main-sidebar">
+
+  <!-- sidebar: style can be found in sidebar.less -->
+  <section class="sidebar">
+
+    <!-- Sidebar user panel (optional) -->
+    <div class="user-panel">
+      <div class="pull-left image">
+        <img src="<?= THEME_DIR ?>assets/img/avatar_small.png" class="img-square" alt="User Image">
+      </div>
+      <div class="pull-left info">
+        <p>Administrator</p>
+        <!-- Status -->
+        <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+      </div>
+    </div>
+
+    <!-- Sidebar Menu -->
+    <ul class="sidebar-menu" data-widget="tree">
+
+      <!-- MODULES -->
+      <li class="header text-uppercase">Modules</li>
+
+      <li class="treeview">
+        <a href="#"><i class="fa fa-files-o"></i> <span>Pages</span>
+          <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+        </a>
+        <ul class="treeview-menu">
+          <li><a href="#"><i class="fa fa-angle-double-right"></i>Link in level 2</a></li>
+          <li><a href="#"><i class="fa fa-angle-double-right"></i>Link in level 2</a></li>
+        </ul>
+      </li>
+
+      <li class="treeview">
+        <a href="#"><i class="fa fa-newspaper-o"></i> <span>News</span>
+          <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+        </a>
+        <ul class="treeview-menu">
+          <li><a href="#">Link in level 2</a></li>
+          <li><a href="#">Link in level 2</a></li>
+        </ul>
+      </li>
+
+      <li class="treeview">
+        <a href="#"><i class="fa fa-list-alt"></i> <span>Forum</span>
+          <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+        </a>
+        <ul class="treeview-menu">
+          <li><a href="#">Link in level 2</a></li>
+          <li><a href="#">Link in level 2</a></li>
+        </ul>
+      </li>
+
+      <li class="treeview">
+        <a href="#"><i class="fa fa-shopping-cart"></i> <span>Shop</span>
+          <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+        </a>
+        <ul class="treeview-menu">
+          <li><a href="#">Link in level 2</a></li>
+          <li><a href="#">Link in level 2</a></li>
+        </ul>
+      </li>
+
+      <li class="treeview">
+        <a href="#"><i class="fa fa-calendar"></i> <span>Events</span>
+          <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+        </a>
+        <ul class="treeview-menu">
+          <li><a href="#">Link in level 2</a></li>
+          <li><a href="#">Link in level 2</a></li>
+        </ul>
+      </li>
+
+      <!-- System -->
+      <li class="header text-uppercase">System</li>
+
+      <li><a href="#"><i class="fa fa-gears"></i> <span>Settings</span></a></li>
+
+    </ul>
+    <!-- /.sidebar-menu -->
+  </section>
+  <!-- /.sidebar -->
+</aside>
